b4977eac1fb75ed34b3829dee88449c4b4190e41
yungpanko <jrjohns@mit.edu>
2017-06-14 10:54:28 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name] == ""
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+end
